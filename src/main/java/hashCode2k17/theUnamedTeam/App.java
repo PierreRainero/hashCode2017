f471ee22bbba69c292be2c5019bc7f48295e7a65
Pierre Rainero <pierre.rainero@etu.unice.fr>
2017-02-23 19:11:46 +0100

    maven created

---
+++ src/main/java/hashCode2k17/theUnamedTeam/App.java
@@ -0,0 +1,13 @@
+package hashCode2k17.theUnamedTeam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

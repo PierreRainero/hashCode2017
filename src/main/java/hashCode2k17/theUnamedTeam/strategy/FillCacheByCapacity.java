795e077edcc7f8f603231d1d083bd79df36d58de
Fabien Durando <fabien.durando@etu.unice.fr>
2017-02-23 21:32:24 +0100

    SearchEndpoitn

--- src/main/java/hashCode2k17/theUnamedTeam/strategy/FillCacheByCapacity.java
+++ src/main/java/hashCode2k17/theUnamedTeam/strategy/FillCacheByCapacity.java
@@ -1,6 +1,6 @@
 package hashCode2k17.theUnamedTeam.strategy;
 
-import hashCode2k17.theUnamedTeam.context.Video;
+import hashCode2k17.theUnamedTeam.context.*;
 
 import java.util.List;
 
@@ -11,11 +11,15 @@ public class FillCacheByCapacity {
         this.context=context.clone();
     }
 
-    public void schedule(){
-        int videoNumber=0;
-        List<Video> videos = context.getServer.getVideo();
+    public String schedule(){
+        List<Video> videos = context.getDataCenter().getVideos();
+        Endpoints endpoint = null;
         for(int i=1;i<videos.size();i++){
-
+            endpoint = videos.get(i).getMostAskedEndPoints();
         }
     }
+/*
+    public String returnedString(){
+
+    }*/
 }

918e800de770db5fd494091f4692804f5dff0f50
Pierre Rainero <pierre.rainero@etu.unice.fr>
2017-02-23 19:40:51 +0100

    dependance

---
+++ src/test/java/hashCode2k17/theUnamedTeam/context/EndpointsTest.java
@@ -0,0 +1,13 @@
+package hashCode2k17.theUnamedTeam.context;
+
+import org.junit.jupiter.api.Test;
+
+public class EndpointsTest {
+	private Endpoints endpoint;
+	
+	@Before
+	public void initContext(){
+		endpoint = new Endpoints();
+	}
+
+}

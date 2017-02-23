19ecef97ae5fde0d86fae9559094637f324a5cef
Fabien Durando <fabien.durando@etu.unice.fr>
2017-02-23 19:38:38 +0100

    vtest

--- src/test/java/hashCode2k17/theUnamedTeam/context/VideoTest.java
+++ src/test/java/hashCode2k17/theUnamedTeam/context/VideoTest.java
@@ -2,10 +2,21 @@ package hashCode2k17.theUnamedTeam.context;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VideoTest {
 
     @Test
     public void testCreate(){
-        Video v = new Video();
+        Endpoints endpoints = new Endpoints(500);
+        Endpoints endpoints1 = new Endpoints(500);
+        Endpoints endpoints2 = new Endpoints(500);
+        Map<Endpoints, Integer> map = new HashMap<Endpoints, Integer>();
+        map.put(endpoints, 1500);
+        map.put(endpoints1, 1500);
+        map.put(endpoints2, 1500);
+        Video v = new Video(500, map);
+
     }
 }
